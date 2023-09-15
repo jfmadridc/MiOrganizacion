7880e425f7c5b9b12756ce6e5c0e84c1d77c1fff
JuanF Madrid <37786333+jfmadridc@users.noreply.github.com>
2023-09-14 22:14:32 -0500

    Creando mi primer componente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./componentes/Header/Header";
+console.log(Header);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>Hello World! I'm using React!!!</p>
+				<a
+					className="App-link"
+					href="https://jfmadrid.com"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Developed by JFM
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
