b02df4bcf70d090385a20c0657f54c161f2184c0
JuanF Madrid <37786333+jfmadridc@users.noreply.github.com>
2023-09-16 10:41:22 -0500

    Centrando titulo Mi Org.. y boton a la derecha end

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import Header from "./componentes/Header/Header.js";
 import Formulario from "./componentes/Formulario/Formulario.js";
-
+import MiOrg from "./componentes/MiOrg/MiOrg.js";
 
 function App() {
 	return (
 		<div>
 			<Header />
 			<Formulario />
+			<MiOrg />
 		</div>
 	);
 }
---
+++ src/componentes/MiOrg/MiOrg.js
@@ -0,0 +1,15 @@
+import "./MiOrg.css";
+
+const MiOrg = () => {
+	return (
+		<div className="div-org">
+			<section className="orgSection">
+				<img src="/img/add.png" alt="boton add" />
+				<h3 className="title">Mi Organizacion</h3>
+				<img src="/img/add.png" alt="boton add" />
+			</section>
+		</div>
+	);
+};
+
+export default MiOrg;
