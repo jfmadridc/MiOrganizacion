bf20fd44b148ad59e8dcfaa57d08290097b849b8
JuanF Madrid <37786333+jfmadridc@users.noreply.github.com>
2023-09-15 23:44:28 -0500

    Evitar recargar la pagina con click boton "Crear"

--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -4,16 +4,20 @@ import ListaOpciones from "../ListaOpciones/ListaOpciones.js";
 import Boton from "../Boton/Boton.js";
 
 const Formulario = () => {
+	const manejarEnvio = (e) => {
+		e.preventDefault();
+		//console.log(e);
+	};
+
 	return (
 		<section className="formulario">
-			<form>
+			<form onSubmit={manejarEnvio}>
 				<h2>Rellena el formulario para crear el colaborador.</h2>
 				<CampoTexto titulo="Nombre" placeholder="Ingrese el nombre" />
 				<CampoTexto titulo="Puesto" placeholder="Ingrese el puesto" />
 				<CampoTexto titulo="Foto" placeholder="Seleccione la foto" />
 				<ListaOpciones />
-				<Boton texto="Crear" />
-
+				<Boton>Crear</Boton>
 			</form>
 		</section>
 	);
