21e9252f007885f612b26db9a580a696d03ee23c
JuanF Madrid <37786333+jfmadridc@users.noreply.github.com>
2023-09-17 11:41:15 -0500

    Creando  states y funciones para escribir en input

--- src/componentes/CampoTexto/CampoTexto.js
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./CampoTexto.css";
 
 const CampoTexto = (props) => {
 	const placeHolderDots = props.placeholder + "...";
+
+	const manejarCambio = (e) => {
+		props.setValor(e.target.value);
+	};
+
 	return (
 		<div className="campo-texto">
 			<label>{props.titulo} </label>
-			<input placeholder={placeHolderDots} required={props.isRequired} />
+			<input
+				placeholder={placeHolderDots}
+				required={props.isRequired}
+				value={props.valor}
+				onChange={manejarCambio}
+			/>
 		</div>
 	);
 };
--- src/componentes/Formulario/Formulario.js
+++ src/componentes/Formulario/Formulario.js
@@ -2,8 +2,13 @@ import "./Formulario.css";
 import CampoTexto from "../CampoTexto/CampoTexto.js";
 import ListaOpciones from "../ListaOpciones/ListaOpciones.js";
 import Boton from "../Boton/Boton.js";
+import { useState } from "react";
 
 const Formulario = () => {
+	const [nombre, actualizarNombre] = useState("");
+	const [puesto, actualizarPuesto] = useState("");
+	const [foto, actualizarFoto] = useState("");
+
 	const manejarEnvio = (e) => {
 		e.preventDefault();
 		//console.log(e);
@@ -17,16 +22,22 @@ const Formulario = () => {
 					titulo="Nombre"
 					placeholder="Ingrese el nombre"
 					isRequired={true}
+					valor={nombre}
+					setValor={actualizarNombre}
 				/>
 				<CampoTexto
 					titulo="Puesto"
 					placeholder="Ingrese el puesto"
 					isRequired
+					valor={puesto}
+					setValor={actualizarPuesto}
 				/>
 				<CampoTexto
 					titulo="Foto"
 					placeholder="Seleccione la foto"
 					isRequired
+					valor={foto}
+					setValor={actualizarFoto}
 				/>
 				<ListaOpciones />
 				<Boton>Crear</Boton>
