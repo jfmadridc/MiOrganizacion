ebe57fa25a7f6ad20827b3b3782b2b3ea235e602
JuanF Madrid <37786333+jfmadridc@users.noreply.github.com>
2023-09-16 20:08:14 -0500

    mejorando estilos responsive

--- src/componentes/MiOrg/MiOrg.js
+++ src/componentes/MiOrg/MiOrg.js
@@ -1,12 +1,20 @@
 import "./MiOrg.css";
 
 const MiOrg = () => {
+	const manejarClick = () => {
+		console.log("Mostrar/Ocultar elemento");
+	};
+
 	return (
 		<div className="div-org">
 			<section className="orgSection">
 				<img src="/img/add.png" alt="boton add" />
 				<h3 className="title">Mi Organizacion</h3>
-				<img src="/img/add.png" alt="boton add" />
+				<img
+					src="/img/add.png"
+					alt="boton add"
+					onClick={manejarClick}
+				/>
 			</section>
 		</div>
 	);
